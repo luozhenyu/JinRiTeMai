e6e6f9b69ef9c98a37bb5b34b29d8dc045b0a633
luozhenyu <lzy@luozy.cn>
2018-11-18 23:12:32 +0800

    Finish product list.

--- src/Product/Client.php
+++ src/Product/Client.php
@@ -11,6 +11,9 @@ use Luozhenyu\JinRiTeMai\Kernel\BaseClient;
  */
 class Client extends BaseClient
 {
+    const PRODUCT_PUT_ON = 0;
+    const PRODUCT_PUT_OFF = 1;
+
     /**
      * @var string
      */
@@ -19,11 +22,31 @@ class Client extends BaseClient
     /**
      * @param int $cid
      * @return array
-     * @throws \Luozhenyu\JinRiTeMai\Kernel\Exception\HttpRequestException
-     * @throws \Luozhenyu\JinRiTeMai\Kernel\Exception\JsonException
      */
     public function getGoodsCategory(int $cid = 0)
     {
         return $this->httpGet('product/getGoodsCategory', compact('cid'));
     }
+
+    /**
+     * @param int $page
+     * @param int $size
+     * @param int|null $status
+     * @return array
+     */
+    public function list(
+        int $page = 0,
+        int $size = 10,
+        $status = null
+    )
+    {
+        $query['page'] = $page;
+        $query['size'] = $size;
+        if (!is_null($status)) {
+            $query['status'] = $status;
+        }
+
+        return $this->httpGet('product/list', $query);
+    }
+
 }
